ae56bd5a36026461867841688f244146658bf6b4
Maksim Makushchenko <maksalmak@gmail.com>
2021-09-29 17:56:46 +0300

    Init java project

---
+++ src/ru/capchik/iep0221/Main.java
@@ -0,0 +1,10 @@
+package ru.capchik.iep0221;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+        System.out.println("Hello world!");
+    }
+}
